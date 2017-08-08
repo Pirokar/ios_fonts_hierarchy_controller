90a127517a176fc26deeb458c8b05bb8287ce2b2
Pirokar <emessage@yandex.ru>
2017-08-08 14:30:54 +0300

    implementd font list

---
+++ Fonts/RootViewController.swift
@@ -0,0 +1,71 @@
+//
+//  RootViewController.swift
+//  Fonts
+//
+//  Created by Владимир Рыбалка on 08/08/2017.
+//  Copyright © 2017 Vladimir Rybalka. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+    
+    private var familyNames: [String]!
+    private var cellPointSize: CGFloat!
+    private var favoritesList: FavoritesList!
+    private let familyCell = "FamilyName"
+    private let favoritesCell = "Favorites"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        familyNames = (UIFont.familyNames as [String]).sorted()
+        let preferredTableViewFont = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        cellPointSize = preferredTableViewFont.pointSize
+        favoritesList = FavoritesList.sharedFavoritesList
+        tableView.estimatedRowHeight = cellPointSize
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    func fontToDisplay(atIndexPath indexPath: IndexPath) -> UIFont? {
+        if indexPath.section == 0 {
+            let familyName = familyNames[indexPath.row]
+            let fontName = UIFont.fontNames(forFamilyName: familyName).first
+            
+            return fontName != nil ? UIFont(name: fontName!, size: cellPointSize) : nil
+        } else {
+            return nil
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return favoritesList.favorites.isEmpty ? 1 : 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? familyNames.count : 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "Все семейства шрифтов" : "Мои избранные шрифты"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            // Список имен шрифтов
+            let cell = tableView.dequeueReusableCell(withIdentifier: familyCell, for: indexPath) as UITableViewCell
+            cell.textLabel?.font = fontToDisplay(atIndexPath: indexPath)
+            cell.textLabel?.text = familyNames[indexPath.row]
+            cell.detailTextLabel?.text = familyNames[indexPath.row]
+            
+            return cell
+        } else {
+            //Список избранных шрифтов
+            return tableView.dequeueReusableCell(withIdentifier: favoritesCell, for: indexPath) as UITableViewCell
+        }
+    }
+}
